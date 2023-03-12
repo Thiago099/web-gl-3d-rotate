435d49774f815291b79e08c14cdbd8bafebf98ef
Thiago <thiago.kaique@stayfilm.com.br>
2023-03-12 16:05:01 -0300

    feature

--- src/index.jsx
+++ src/index.jsx
@@ -67,19 +67,22 @@ void main(void) {
     // Apply lighting effect
 
     highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-    highp vec3 directionalLightColor = vec3(1, 1, 1);
-    highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+    highp vec3 frontColor = vec3(1, 1, 1);
+    highp vec3 backColor = vec3(.8, .8, .8);
+    highp vec3 frontVector = normalize(vec3(0.85, 0.8, 0.75));
+    highp vec3 backVector = normalize(vec3(-0.85, -0.8, -0.75));
 
     highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
 
-    highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+    highp float directional = max(dot(transformedNormal.xyz, frontVector), 0.0);
+    highp float directional2 = max(dot(transformedNormal.xyz, backVector), 0.0);
     if(isPickingStep == 0.0)
     {
         vLighting = vec3(1.0, 1.0, 1.0);
     }
     else
     {
-        vLighting = ambientLight + (directionalLightColor * directional);
+        vLighting =  ambientLight + (frontColor * directional) + (backColor * directional2);
     }
 }
 `;
