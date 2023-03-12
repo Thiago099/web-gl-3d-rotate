4fad8713fb3f0b190ba4758f49b77ac333820593
Thiago <thiago.kaique@stayfilm.com.br>
2023-03-12 16:23:02 -0300

    feature

--- src/index.jsx
+++ src/index.jsx
@@ -70,8 +70,10 @@ void main(void) {
     // Apply lighting effect
 
     highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+    
     highp vec3 frontColor = vec3(1, 1, 1);
     highp vec3 backColor = vec3(.8, .8, .8);
+
     highp vec3 frontVector = normalize(vec3(0.85, 0.8, 0.75));
     highp vec3 backVector = normalize(vec3(-0.85, -0.8, -0.75));
 
@@ -217,32 +219,32 @@ canvas.$on("wheel", mouseWheel);
 /*=========================rotation================*/
 
 function rotateX(m, angle) {
-var c = Math.cos(angle);
-var s = Math.sin(angle);
-var mv1 = m[1], mv5 = m[5], mv9 = m[9];
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+    var mv1 = m[1], mv5 = m[5], mv9 = m[9];
 
-m[1] = m[1]*c-m[2]*s;
-m[5] = m[5]*c-m[6]*s;
-m[9] = m[9]*c-m[10]*s;
+    m[1] = m[1]*c-m[2]*s;
+    m[5] = m[5]*c-m[6]*s;
+    m[9] = m[9]*c-m[10]*s;
 
-m[2] = m[2]*c+mv1*s;
-m[6] = m[6]*c+mv5*s;
-m[10] = m[10]*c+mv9*s;
+    m[2] = m[2]*c+mv1*s;
+    m[6] = m[6]*c+mv5*s;
+    m[10] = m[10]*c+mv9*s;
 }
 
 function rotateY(m, angle) {
-var c = Math.cos(angle);
-var s = Math.sin(angle);
-var mv0 = m[0], mv4 = m[4], mv8 = m[8];
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+    var mv0 = m[0], mv4 = m[4], mv8 = m[8];
 
-m[0] = c*m[0]+s*m[2];
-m[4] = c*m[4]+s*m[6];
-m[8] = c*m[8]+s*m[10];
+    m[0] = c*m[0]+s*m[2];
+    m[4] = c*m[4]+s*m[6];
+    m[8] = c*m[8]+s*m[10];
 
-m[2] = c*m[2]-s*mv0;
-m[6] = c*m[6]-s*mv4;
-m[10] = c*m[10]-s*mv8;
-// check is the orientation is greater than 90 degrees
+    m[2] = c*m[2]-s*mv0;
+    m[6] = c*m[6]-s*mv4;
+    m[10] = c*m[10]-s*mv8;
+    // check is the orientation is greater than 90 degrees
 
 }
 
@@ -267,9 +269,6 @@ function draw()
     addNormals(gl,_VertexNormal) 
     
     GlDraw(gl)
-    
-    
-    
 }
 
 var animate = function(time) {
